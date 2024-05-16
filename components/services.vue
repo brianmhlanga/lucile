bd8d5ef8be54775f3b76b5db07bca0feac315ad1
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-16 23:26:22 +0200

    created services section

---
+++ components/services.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="block-content">
+  <div class="">
+    <div class="px-4 py-4 md:px-6 lg:px-8" style="background: radial-gradient(69.84% 69.84% at 50% 100%, rgba(21, 101, 192, 0.15) 0%, rgba(255, 255, 255, 0) 100%);">
+        <div class="topcurated  px-4 py-4 md:px-6 lg:px-8">
+      <div class="font-bold goTo text-3xl toptext text-center">What are you looking for?</div>
+      <div class="text-700 text-center mb-5 line-height-3">We are one of the largest Real Estate Agency in Zimbabwe</div>
+    </div>
+      <div class="grid mt-7">
+        <div class="col-12 md:col-3">
+          <div class="shadow-1 p-5 blackcard surface-card text-center border-round">
+            <div class="border-circle bg-blueff-50 p-4 text-blue-600 inline-flex justify-content-center align-items-center mb-4">
+              <i class="pi pi-building-columns text-5xl"></i>
+            </div>
+            <div class="whitee text-900 font-medium text-2xl mb-4">Sales</div>
+            <div class="whitee text-700 mb-4 line-height-3">Experience the magic of finding your dream home with Lucile Real Estate. Our wide range of properties for sale across Zimbabwe ensures that your ideal home is just a click away. From charming apartments to elegant estates</div>
+          </div>
+        </div>
+        <div class="col-12 md:col-3">
+          <div class="shadow-1 p-5 goldcard surface-card text-center border-round">
+            <div class="border-circle bg-purple-5rty0 p-4 text-purple-600 inline-flex justify-content-center align-items-center mb-4">
+              <i class="pi pi-warehouse text-5xl"></i>
+            </div>
+            <div class="whitee text-900 font-medium text-2xl mb-4">Rentals</div>
+            <div class="whitee text-700 mb-4 line-height-3">Looking for your ideal rental property? Lucile Real Estate has a range of options to meet your needs, whether you're looking for a short-term stay or a long-term home. Choose from our diverse selection of properties for the perfect rental experience.</div>
+          </div>
+        </div>
+        <div class="col-12 md:col-3">
+          <div class="shadow-1 p-5 blackcard surface-card text-center border-round">
+            <div class="border-circle bg-blueff-50  p-4 text-orange-600 inline-flex justify-content-center align-items-center mb-4">
+              <i class="pi pi-calendar-clock text-5xl"></i>
+            </div>
+            <div class="whitee text-900 font-medium text-2xl mb-4">Show Days</div>
+            <div class="whitee text-700 mb-4 line-height-3">Planning to buy a home but unsure where to find all the available properties in your area? Let Lucile Real Estate guide you. Plan your tour with us to explore all the homes for sale in your desired location.Browse <a>Showdays</a> for more</div>
+          </div>
+        </div>
+        <div class="col-12 md:col-3">
+          <div class="shadow-1 p-5 goldcard surface-card text-center border-round">
+            <div class="border-circle bg-purple-5rty0 p-4 text-orange-600 inline-flex justify-content-center align-items-center mb-4">
+              <i class="pi pi-chart-line text-5xl"></i>
+            </div>
+            <div class="whitee headerr text-900 font-medium text-2xl mb-4">Market Appraisal</div>
+            <div class="whitee text-700 mb-4 line-height-3">Starting the next chapter of your story by selling your house? Begin with a market appraisal, the crucial first step toward putting your property on the market. Let Lucile Real Estate guide you through this process and start your new chapter.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!---->
+</div>
+</template>
+<style>
+.goldcard {
+    background-color: #ecc36b !important;
+}
+.whitee {
+    color: white !important;
+}
+.blackcard {
+    background-color: rgb(0, 0, 0) !important;
+}
+.bg-blueff-50 {
+    background-color: #ecc36b;
+    color: white !important;
+}
+.bg-purple-5rty0 {
+    background-color: black;
+    color: white !important;
+}
+
+</style>
