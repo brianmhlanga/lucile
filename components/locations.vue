04e170554c37af3b10f3e78a665a8fcd4b6ea9c0
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-16 23:25:54 +0200

    added locations page

---
+++ components/locations.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="block-content">
+  <div class="">
+    <div class="surface-ground px-4 py-8 md:px-6 lg:px-8">
+        <div class="topcurated  px-4 py-4 md:px-6 lg:px-8">
+      <div class="font-bold goTo text-3xl toptext text-center">Locations</div>
+      <div class="text-700 text-center mb-5 line-height-3">Dreaming of owning or renting a property in Zimbabwe's major cities?</div>
+    </div>
+      <div class="grid grid-nogutter">
+        <div class="col-12 lg:col-4 p-3">
+          <div class="p-3 shadow-2 border-round surface-card h-full">
+            <div class="flex justify-content-between text-500">
+              <span class="city">Harare</span>
+              <span class="inline-flex">
+                <i class="pi pi-home mr-2"></i>
+                <span>12 Properties</span>
+              </span>
+            </div>
+            <img src="/images/harare.png" alt="Image" class="block w-full mt-5">
+          </div>
+        </div>
+        <div class="col-12 lg:col-4 p-3">
+          <div class="p-3 shadow-2 border-round surface-card h-full">
+            <div class="flex justify-content-between text-500">
+              <span class="city">Bulawayo</span>
+              <span class="inline-flex">
+                <i class="pi pi-home mr-2"></i>
+                <span>9 Properties</span>
+              </span>
+            </div>
+            <img src="/images/bulawayo.png" alt="Image" class="block w-full mt-5">
+          </div>
+        </div>
+        <div class="col-12 lg:col-4 p-3">
+          <div class="p-3 shadow-2 border-round surface-card h-full">
+            <div class="flex justify-content-between text-500">
+              <span class="city">Gweru</span>
+              <span class="inline-flex">
+                <i class="pi pi-home mr-2"></i>
+                <span>5 Properties</span>
+              </span>
+            </div>
+            <img src="/images/gweru.png" alt="Image" class="block w-full mt-5">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!---->
+</div>
+  </template>
+  <style>
+  span.city {
+      background-color: #ecc36b;
+      padding: 10px;
+      color: white !important;
+      border-radius: 5px;
+  }
+  </style>
+  
+<style>
+span.city {
+    background-color: #ecc36b;
+    padding: 10px;
+    color: white !important;
+    border-radius: 5px;
+}
+</style>
