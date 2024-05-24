9feef51ac540f24bd77cfd46237958b3de894f36
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-24 10:38:42 +0200

    updated all properties endpoint

--- server/routes/admin/all_properties.ts
+++ server/routes/admin/all_properties.ts
@@ -6,7 +6,15 @@ let response:any = {}
 
 try {
 
-    let properties = await prisma.property.findMany()
+    let properties = await prisma.property.findMany({
+        include: {
+            features: true,
+            agent: true,
+            location: true,
+            suburb: true,
+            prop_type: true
+        }
+    })
     response['properties'] = properties
     response['success'] = true
 
