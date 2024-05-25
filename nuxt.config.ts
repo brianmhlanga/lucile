2c8457f4d4fd596f9f385117f2c5e272c3558b48
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-25 12:31:55 +0200

    added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-primevue',"@pinia/nuxt", "@nuxt/image","nuxt-swiper"],
+  modules: [
+    'nuxt-primevue',
+    "@pinia/nuxt",
+    "@nuxt/image",
+    "nuxt-swiper"
+  ],
   swiper: {
     // Swiper options
     //----------------------
