f1ddc2518444a8bc27ae5f06d95686cc801880c5
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-25 12:32:47 +0200

    added about us

---
+++ pages/about_us.vue
@@ -0,0 +1,82 @@
+<template>
+    <NuxtLayout name="main" >
+        <div class="block-content aboutsection">
+  <div class="">
+    <div class="surface-section aboutsection px-4 py-8 md:px-6 lg:px-8">
+      <div class="flex flex-wrap">
+        <div class="w-full xl:w-6 p-3 pr-5">
+          <span class="text-indigo-600 font-bold yelloy mb-4 block">DREAM IT | SEE IT | LIVE IT</span>
+          <div class="font-bold text-900 text-6xl yelloy mb-4">About Lucile Real Estate</div>
+          <p class="mt-0 mb-5 text-700 line-height-3 yelloy text-xl">Lucile Real Estate is a locally incorporated company in the field of Property Sales, Real Estate Portfolio Management and Advisory, Project Management, Construction, Renovations and Interior Design.</p>
+
+        <p class="mt-0 mb-5 text-700 line-height-3 yelloy text-xl">We have built our reputation on financial strength, the ability to respond quickly to opportunities in the market place and a history of positive performance on behalf of our clients.</p>
+
+        <p class="mt-0 mb-5 text-700 line-height-3 yelloy text-xl">Our company is not confined to traditional real estate boundaries when it comes to buying, selling and property management, instead we are always coming up with new innovative ways to satisfy our client's need and be the leading company in the real estate industry.</p>
+
+        <p class="mt-0 mb-5 text-700 line-height-3 yelloy text-xl">Our team is hands-on when it comes to visiting the sites for approval to make sure that all properties are in good condition, location and perfect view before showing it to the clients. We also work continually to interpret and act upon economic forces in the marketplace. And with every transaction, we stress on creative solving coupled with judicious application of sound real estate principles.</p>
+
+        </div>
+        <div class="w-full lg:w-6 pl-0 lg:pl-5 pt-5">
+  <img src="/images/interior.jpg" alt="Image" class="w-full border-round">
+  <div class="text-center text-sm text-caption font-medium mt-3">Renovations & Interior Design</div>
+  <img src="/images/house.jpg" alt="Image" class="w-full border-round">
+  <div class="text-center text-sm text-caption font-medium mt-3">Property Sales & Rentals</div>
+</div>
+      </div>
+    </div>
+  </div>
+  <!---->
+</div>
+    </NuxtLayout>
+ 
+</template>
+<style>
+.pnames {
+    font-size: 30px !important;
+    font-weight: 600 !important;
+}
+div#descriptionContainer {
+    max-height: calc(1.2em* 8);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-bottom: 10px;
+    white-space: pre-line;
+}
+span.location.wises {
+    font-size: 20px;
+    font-weight: 200;
+}
+span.text-500.sale.text-lg.font-medium {
+    background-color: #ecc36b;
+    color: white !important;
+    padding: 5px;
+    border-radius: 2px;
+}
+span.text-orange-500.font-medium {
+    color: black !important;
+    font-size: 25px;
+}
+.ghf {
+    float: right;
+    background-color: #27303d;
+    border: none;
+    height: 45px;
+    justify-content: space-between;
+}
+.sort-by {
+    display: flex;
+    margin-bottom: 1rem;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+}
+.aboutsection {
+    background-color: #27303d !important;
+}
+.yelloy {
+    color: #ebc562 !important;
+}
+.text-center.text-sm.text-caption.font-medium.mt-3 {
+    color: white;
+}
+</style>
