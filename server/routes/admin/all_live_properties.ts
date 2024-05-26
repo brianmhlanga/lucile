6539813dead41112d4d89ed116c081f6a284335a
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-26 15:39:11 +0200

    updated all live endpoint

--- server/routes/admin/all_live_properties.ts
+++ server/routes/admin/all_live_properties.ts
@@ -3,8 +3,13 @@ import {prisma } from "~/prisma/db"
 
 export default defineEventHandler( async (event) => {
 let response:any = {}
-
+let {status,sort,first,last} = await readBody(event)
 try {
+    let prop_count = await prisma.property.count({
+        where: {
+            status: "LISTED"
+        }
+    })
 
     let properties = await prisma.property.findMany({
         where: {
@@ -16,9 +21,16 @@ try {
             location: true,
             suburb: true,
             prop_type: true
-        }
+        },
+        orderBy: {
+            created_at: "desc"
+        },
+         skip: first,
+         take: 4
+
     })
     response['properties'] = properties
+    response['total'] = prop_count
     response['success'] = true
 
 } catch (error:any) {
