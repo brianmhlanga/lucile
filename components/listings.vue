c3f6ee6d2632db5b5472811928af76acd0f99b68
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-26 15:37:08 +0200

    updated listings page

--- components/listings.vue
+++ components/listings.vue
@@ -1,62 +1,77 @@
 <template>
-    <div class="topcurated  px-4 py-4 md:px-6 lg:px-8">
-      <div class="font-bold goTo text-3xl toptext text-center">Featured Properties</div>
-      <div class="text-700 text-center mb-5 line-height-3">Curated list of the best real estate listings available</div>
-    </div>
-    <div class="surface-ground px-4 py-8 md:px-6 lg:px-8">
-      <div class="grid -mt-3 -ml-3 -mr-3">
-  <div v-for="property in properties" class="col-12 md:col-4 lg:col-3">
-    <div class="p-2">
-      <div class="shadow-2 p-4 surface-card border-round">
-        <div class="relative mb-3">
-          <span v-if="property?.listing_status === 'FOR_SALE'" class=" for_sale surface-card listing_type text-900 shadow-2 px-3 py-2 absolute" style="border-radius: 1.5rem; left: 1rem; top: 1rem;">FOR SALE</span>
-          <span v-if="property?.listing_status === 'FOR_RENT'"  class="for_rent surface-card listing_type text-900 shadow-2 px-3 py-2 absolute" style="border-radius: 1.5rem; left: 1rem; top: 1rem;">FOR RENT</span>
-          <Galleria :value="property?.images" :showThumbnails="false" :showIndicators="true" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px"
-            :circular="true" :autoPlay="true" :transitionInterval="2000">
-            <template #item="slotProps">
-                <img :src="getLink(slotProps.item.image_url)" style="width: 100%; display: block" />
-            </template>
-        </Galleria>
+  <div class="topcurated px-4 py-4 md:px-6 lg:px-8">
+    <div class="font-bold goTo text-3xl toptext text-center">Featured Properties</div>
+    <div class="text-700 text-center mb-5 line-height-3">Curated list of the best real estate listings available</div>
+  </div>
+  <div class="surface-ground px-4 md:px-6 lg:px-8 card-deck">
+    <div v-for="property in properties" :key="property.id" class="col-12 md:col-4 lg:col-3 h-100">
+      <div class="p-2">
+        <div class="shadow-2 p-4 surface-card border-round">
+          <div class="relative mb-3">
+            <span v-if="property?.listing_status === 'FOR_SALE'" class="for_sale surface-card listing_type text-900 shadow-2 px-3 py-2 absolute" style="border-radius: 1.5rem; left: 1rem; top: 1rem;">FOR SALE</span>
+            <span v-if="property?.listing_status === 'FOR_RENT'" class="for_rent surface-card listing_type text-900 shadow-2 px-3 py-2 absolute" style="border-radius: 1.5rem; left: 1rem; top: 1rem;">FOR RENT</span>
+            <Galleria :value="property?.images" :showThumbnails="false" :showIndicators="true" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px"
+              :circular="true" :autoPlay="true" :transitionInterval="2000">
+              <template #item="slotProps">
+                <img :src="getLink(slotProps.item.image_url)" class="imagessonhome w-full" style="width: 100%; display: block" />
+              </template>
+            </Galleria>
+          </div>
+          <div class="flex justify-content-between align-items-center mb-3">
+            <span class="text-900 font-medium text-xl">{{ property?.name }}<br><span class="inline-flex align-items-center mb-3 loc"><span class="material-icons tex-600 mr-2">location_on</span><span class="text-600 locname">{{ property?.address }}, {{ property?.location?.location_name }}</span></span></span>
+          </div>
+          <p class="mt-0 mb-3 text-600 line-height-3 description" v-html="property?.description"></p>
+          <div class="border-bottom-1 surface-border my-3 py-2">
+            <div class="flex justify-content-between mb-3">
+              <span class="inline-flex align-items-center mb-3"><span class="material-icons tex-600 mr-2">bed</span><span class="text-600 mr-2">Bedrooms</span></span>
+              <span class="text-900">{{ property?.features[0].total ? property?.features[0].total : 0 }}</span>
+            </div>
+            <div class="flex justify-content-between mb-3">
+              <span class="inline-flex align-items-center mb-3"><span class="material-icons tex-600 mr-2">shower</span><span class="text-600 mr-2">Bathrooms</span></span>
+              <span class="text-900">{{ property?.features[1].total ? property?.features[1].total : 0 }}</span>
+            </div>
+            <div class="flex justify-content-between mb-3">
+              <span class="inline-flex align-items-center mb-3"><span class="material-icons tex-600 mr-2">window</span><span class="text-600 mr-2">Area</span></span>
+              <span class="text-900">{{ property?.features[2].total ? property?.features[2].total : 0 }} Square Meters</span>
+            </div>
+          </div>
+          <div class="flex align-items-center mb-3">
+            
+          </div>
+          <span class="text-primary text-xl font-medium price">{{ formatCurrency(property?.amount) }}</span>
+          <Button @click="navigateTo(`/detail-${property?.id}`)" class="p-component detail-button cursor-pointer p-3 surface-900 hover:surface-800 border-none text-0 w-full transition-all transition-duration-300 text-center p-ripple" label="Details" />
         </div>
-        <div class="flex justify-content-between align-items-center mb-3">
-          <span class="text-900 font-medium text-xl">{{ property?.name }}<br><span class="location"><i class="pi pi-map-marker"></i>{{ property?.address }}</span></span>
-          <span>
-            <span class="font-medium">{{ property?.location?.location_name }}</span>
-          </span>
-        </div>
-        <p class="mt-0 mb-3 text-600 line-height-3">{{ property?.description }}</p>
-        <div class="flex align-items-center mb-3">
-          <div class="tyy">
-              <span>Bedrooms</span>
-              <div><i class="pi pi-objects-column ttut"></i><span>{{ property?.features[0].total ? property?.features[0].total : 0 }}</span></div>
-            </div>
-            <div class="tyy">
-              <span>Bathrooms</span>
-              <div><i class="pi pi-hourglass ttut"></i><span>{{ property?.features[1].total ? property?.features[1].total : 0 }}</span></div>
-            </div>
-            <div class="tyy">
-              <span>Area</span>
-              <div>
-                <i class="pi pi-expand ttut"></i><span>{{ property?.features[2].total ? property?.features[2].total : 0 }}<span> Sq Mtrs</span></span>
-              </div>
-            </div>
-       </div>
-        <span class="text-primary text-xl font-medium">{{ formatCurrency(property?.amount) }}</span>
-        <Button @click="navigateTo(`/detail-${property?.id}`)" class="p-component detail-button cursor-pointer p-3 surface-900 hover:surface-800 border-none text-0 w-full transition-all transition-duration-300 text-center p-ripple" label="Details" />
-       </div>
+      </div>
     </div>
   </div>
-</div>
-    </div>
+  <Paginator  @page="getAllProperties()" v-model:first="first" v :rows="items_per_page" :totalRecords="number_of_records ? number_of_records : 0"></Paginator>
 </template>
 <script lang="ts" setup>
 const adminStore = useAdminStore()
 let properties = ref()
+let number_of_records = ref()
+const first = ref(0)
+const items_per_page = ref(4)
+
+const getAllProperties = async() => {
+    let data = {
+      first: first.value,
+      last: items_per_page.value
+    }
+    let result = await adminStore.getLiveProperties(data).then((data) => {
+      properties.value = data?.data?.properties
+      number_of_records.value = data?.data?.total
+    })
+}
 
 onMounted(async() => {
-
-  let results = await adminStore.getLiveProperties().then((data) => {
+  let data = {
+      first: 0,
+      last: items_per_page.value
+    }
+  let results = await adminStore.getLiveProperties(data).then((data) => {
     properties.value = data?.data?.properties
+    number_of_records.value = data?.data?.total
   })
 })
 const formatCurrency = (value:any) => {
@@ -88,8 +103,32 @@ body {
   font-family: Arial, Helvetica, sans-serif;
   background-color: #eee;
 }
+.property-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+span.text-600.locname {
+    font-size: 12px;
+    font-weight: 600;
+}
+.price {
+    background-color: #ecc36b;
+    color: white !important;
+    padding: 2px;
+    border-radius: 5px;
+    font-size: 22px !important;
+    border-left: 8px #27303d solid;
+}
+.property-card {
+  flex: 1;
+}
 .topcurated {
     margin-top: 60px !important;
+}
+.card-deck {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
 }
 .ttut {
     font-weight: 300;
@@ -97,7 +136,7 @@ body {
     margin-right: 8px;
 }
 span.text-900.font-medium.text-xl {
-    font-size: 23px !important;
+    font-size: 17px !important;
     font-weight: 800 !important;
 }
 span.location {
@@ -123,6 +162,13 @@ span.for_sale {
     margin: 100px auto;
     display: flex;
     justify-content: space-between;
+}
+p.mt-0.mb-3.text-600.line-height-3.description {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
 }
 .tyy {
     margin-right: 35px;
