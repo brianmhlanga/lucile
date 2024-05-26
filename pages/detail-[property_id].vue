f09ced73e6209fc1cffa6fb890b7451c17340f92
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-05-26 09:28:10 +0200

    added share links

--- pages/detail-[property_id].vue
+++ pages/detail-[property_id].vue
@@ -75,8 +75,14 @@
             <div class="flex align-items-center justify-content-between mb-3">
               <span class="text-900 font-medium">{{ property?.agent?.first_name }} {{ property?.agent?.surname }}</span>
             </div>
-            <div @click="callAgent(property?.agent?.phone_number)" class="text-600 text-sm mb-3"><i class="pi pi-phone" style="font-size: 1rem"></i> {{ property?.agent?.phone_number }}</div>
-            <div class="text-600 text-sm mb-3"><i class="pi pi-whatsapp" style="font-size: 1rem"></i> {{ property?.agent?.whatsapp_number }}</div>
+            <div @click="callAgent(property?.agent?.phone_number)" class="cursor-pointer text-600 text-sm mb-3"><i class="pi pi-phone" style="font-size: 1rem"></i> {{ property?.agent?.phone_number }}</div>
+            <div class="text-600 text-sm mb-3 cursor-pointer" @click="navigateTo(`https://wa.me/${property.agent.whatsapp_number}/?text=${encodeURIComponent(`Hi ${property.agent.first_name}, I wanted to enquire about ${property?.name}(${property.refrence_code}) listed on https://lucilerealestate.co.zw/detail-${property_id}, feel free to callback for a chat.`)}`,{open: {
+    target: '_blank',
+    windowFeatures: {
+      width: 500,
+      height: 500
+    }
+  }})"><i  class="pi pi-whatsapp" style="font-size: 1rem"></i> {{ property?.agent?.whatsapp_number }}</div>
           </div>
         </div>
         <div class="py-2 mt-3 border-bottom-1 surface-border">
@@ -97,7 +103,7 @@
             
           </div>
           <div class="p-password p-component p-inputwrapper p-input-icon-right w-full mb-2" data-pc-name="password" data-pc-section="root">
-            <Textarea placeholder="Your message"  rows="5" cols="50" />
+            <Textarea v-model="message" placeholder="Your message"  rows="5" cols="50" />
             <!---->
             
           </div>
@@ -108,6 +114,18 @@
           <!---->
           <span role="presentation" aria-hidden="true" data-p-ink="true" data-p-ink-active="false" class="p-ink" data-pc-name="ripple" data-pc-section="root"></span>
         </button>
+      </div>
+      <div class="surface-card border-round mb-3 mt-5 shadow-2 p-5">
+        <div class="flex justify-content-between align-items-center border-bottom-1 mb-3 pb-3 surface-border">
+          <span class="text-900 font-medium text-lg lg:text-xl">
+            <i class="pi pi-share-alt text-xl mr-2"></i>Share</span>
+        </div>
+        <div class="flex align-items-center mb-5">
+          <div @click="shareOnWhatsapp(property?.id,property?.name,property?.listing_status,property?.location?.location_name)" class="h-2rem w-2rem sm:h-3rem sm:w-3rem border-1 border-300 text-900 inline-flex justify-content-center align-items-center flex-shrink-0 border-round mr-3 cursor-pointer hover:surface-100 transition-duration-150 transition-colors"> <i class="pi pi-whatsapp" style="color: #00c722;font-size: 1.5rem;"></i> </div>
+          <div @click="shareOnFacebook(property?.id,property?.name,property?.listing_status,property?.location?.location_name)" class="h-2rem w-2rem sm:h-3rem sm:w-3rem border-1 border-300 text-900 inline-flex justify-content-center align-items-center flex-shrink-0 border-round mr-3 cursor-pointer hover:surface-100 transition-duration-150 transition-colors"> <i class="pi pi-facebook" style="color: #2203ff;font-size: 1.5rem;"></i> </div>
+          <div @click="shareOnTwitter(property?.id,property?.name,property?.listing_status,property?.location?.location_name)" class="h-2rem w-2rem sm:h-3rem sm:w-3rem border-1 border-300 text-900 inline-flex justify-content-center align-items-center flex-shrink-0 border-round mr-3 cursor-pointer hover:surface-100 transition-duration-150 transition-colors"> <i class="pi pi-twitter" style="color: #000000;font-size: 1.5rem;"></i> </div>
+          <div @click="shareViaEmail(property?.id,property?.name,property?.listing_status,property?.location?.location_name)" class="h-2rem w-2rem sm:h-3rem sm:w-3rem border-1 border-300 text-900 inline-flex justify-content-center align-items-center flex-shrink-0 border-round mr-3 cursor-pointer hover:surface-100 transition-duration-150 transition-colors"> <i class="pi pi-envelope" style="color: #ecc36b;font-size: 1.5rem;"></i> </div>
+        </div>
       </div>
     </div>
   </div>
@@ -123,13 +141,14 @@
 const {params:{property_id}} = useRoute()
 const adminStore = useAdminStore()
 const property = ref()
-
+const message = ref()
 onMounted(async() => {
     let data = {
       id: property_id
     }
-    let result = await adminStore.getProperty(data).then((data) => {
+    let result = await adminStore.getProperty(data).then(async (data) => {
       property.value = data?.data?.properties
+      await createMessage(data?.data?.properties?.agent?.first_name,data?.data?.properties?.name, data?.data?.properties?.refrence_code)
     })
 })
 const responsiveOptions = ref([
@@ -142,6 +161,39 @@ const responsiveOptions = ref([
         numVisible: 1
     }
 ]);
+const shareOnFacebook = (id,name,listingstatus,location) => {
+    let text = `Hi, check out this property - ${name} ${listingstatus === 'FOR_SALE' ? "For Sale in" : "For Rent in"} ${location}, Click the link below to view it`;
+    let link = `https://lucilerealestate/detail-${id}`;
+    const facebookIntentURL = "https://www.facebook.com/sharer/sharer.php";
+    const contentQuery = `?u=${encodeURIComponent(link)}&description=${encodeURIComponent(text)}`;
+    const shareURL = facebookIntentURL + contentQuery;
+    window.open(shareURL, "_blank");
+};
+const shareOnTwitter = (id,name,listingstatus,location) => {
+        let text = `Hi, check out this property - ${name} ${listingstatus === 'FOR_SALE' ? "For Sale in" : "For Rent in"} ${location}, Click the link below to view it`
+        let link = `https://lucilerealestate/detail-${id}`
+        const twitterIntentURL = "https://twitter.com/intent/tweet";
+        const contentQuery = `?url=${encodeURIComponent(link)}&text=${encodeURIComponent(text)}`;
+        const shareURL = twitterIntentURL + contentQuery;
+        window.open(shareURL, "_blank");
+}
+const shareViaEmail = (id, name, listingStatus, location) => {
+    let subject = `Check out this property - ${name}`;
+    let body = `Hi, \n\nCheck out this property - ${name} ${listingStatus === 'FOR_SALE' ? "For Sale in" : "For Rent in"} ${location}, Click the link below to view it:\n\n`;
+    let link = `https://lucilerealestate/detail-${id}`;
+    let email = "mailto:?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body + link);
+    window.open(email, "_blank");
+}
+
+const shareOnWhatsapp = (id,name,listingstatus,location) => {
+        let text = `Hi, check out this property - ${name} ${listingstatus === 'FOR_SALE' ? "For Sale in" : "For Rent in"} ${location}, Click the link below to view it`
+        let link = `https://lucilerealestate/detail-${id}`
+        const url = "https://wa.me/?text=" + encodeURIComponent(text + "\n" + link);
+        window.open(url, "_blank");
+}
+const createMessage = async (name,property,ref) => {
+   message.value = `Hi ${name}, can you call back about ${property}(${ref})`
+}
 
 const callAgent = async (pn:any) => {
   console.log("pn",pn)
